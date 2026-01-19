b4f0aa07889771209394b60e7a27340be5606968
DEEPESH-845 <dkakkar_be23@thapar.edu>
2026-01-19 16:36:35 +0530

    Configured routes and enhanced ui

--- src/components/theme-selector.tsx
+++ src/components/theme-selector.tsx
@@ -55,50 +55,62 @@ export function ThemeSelector() {
   const { activeTheme, setActiveTheme } = useThemeConfig()
 
   return (
-    <div className="flex items-center gap-2">
-      <Label htmlFor="theme-selector" className="sr-only">
-        Theme
-      </Label>
-      <Select value={activeTheme} onValueChange={setActiveTheme}>
-        <SelectTrigger
-          id="theme-selector"
-          size="sm"
-          className="justify-start *:data-[slot=select-value]:w-12"
-        >
-          <span className="text-muted-foreground hidden sm:block">
-            Select a theme:
-          </span>
-          <span className="text-muted-foreground block sm:hidden">Theme</span>
-          <SelectValue placeholder="Select a theme" />
-        </SelectTrigger>
-        <SelectContent align="end">
-          <SelectGroup>
-            <SelectLabel>Default</SelectLabel>
-            {DEFAULT_THEMES.map((theme) => (
-              <SelectItem key={theme.name} value={theme.value}>
-                {theme.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-          <SelectSeparator />
-          <SelectGroup>
-            <SelectLabel>Scaled</SelectLabel>
-            {SCALED_THEMES.map((theme) => (
-              <SelectItem key={theme.name} value={theme.value}>
-                {theme.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-          <SelectGroup>
-            <SelectLabel>Monospaced</SelectLabel>
-            {MONO_THEMES.map((theme) => (
-              <SelectItem key={theme.name} value={theme.value}>
-                {theme.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    </div>
-  )
+		<div className="flex items-center  gap-2">
+			<Label htmlFor="theme-selector" className="sr-only ">
+				Theme
+			</Label>
+			<Select value={activeTheme} onValueChange={setActiveTheme}>
+				<SelectTrigger
+					id="theme-selector"
+					size="sm"
+					className="justify-start *:data-[slot=select-value]:w-12"
+				>
+					<span className="hidden sm:block">Select a theme:</span>
+					<span className="block sm:hidden">Theme</span>
+					<SelectValue placeholder="Select a theme" />
+				</SelectTrigger>
+				<SelectContent
+					align="end"
+					className="!bg-zinc-800 !border-zinc-300 !text-zinc-00 !hover:bg-amber-400"
+				>
+					<SelectGroup>
+						{DEFAULT_THEMES.map((theme) => (
+							<SelectItem
+								key={theme.name}
+								value={theme.value}
+								className="focus:bg-zinc-700 focus:text-white"
+							>
+								{theme.name}
+							</SelectItem>
+						))}
+					</SelectGroup>
+					<SelectSeparator className="bg-zinc-600" />
+					<SelectGroup>
+						<SelectLabel>Scaled</SelectLabel>
+						{SCALED_THEMES.map((theme) => (
+							<SelectItem
+								key={theme.name}
+								value={theme.value}
+								className="focus:bg-zinc-700 focus:text-white"
+							>
+								{theme.name}
+							</SelectItem>
+						))}
+					</SelectGroup>
+					<SelectGroup>
+						<SelectLabel>Monospaced</SelectLabel>
+						{MONO_THEMES.map((theme) => (
+							<SelectItem
+								key={theme.name}
+								value={theme.value}
+								className="focus:bg-zinc-700 focus:text-white"
+							>
+								{theme.name}
+							</SelectItem>
+						))}
+					</SelectGroup>
+				</SelectContent>
+			</Select>
+		</div>
+	);
 }
